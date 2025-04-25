819a28cb0dff88cc4f66008d71ff0d0e567c416b
Eduardo <198397564+echo-bravo@users.noreply.github.com>
2025-04-25 16:59:02 -0300

    create homePage script

---
+++ src/pages/homePage.js
@@ -0,0 +1,44 @@
+import { createElement, createNestedElement, setPageContent } from "./pageModule";
+
+const logo = {
+    type: "div",
+    content: [
+        {
+            type: "div",
+            content: "Delicia",
+            classes: ["logo-upper-text"]
+        },
+        {
+            type: "div",
+            content: "na Brasa",
+            classes: ["logo-bottom-text"]
+        }
+    ],
+    classes: ["home-page-logo"]
+};
+
+const description = {
+    type: "p",
+    content: "Comida feita na brasa e com muito carinho!",
+    classes: ["home-page-description"]
+};
+
+const menuButton = {
+    type: "button",
+    content: "Abrir Menu",
+    classes: ["open-menu-button"]
+};
+
+const page = {
+    type: "div",
+    content: [
+        logo,
+        description,
+        menuButton
+    ],
+    classes: ["home-page"]
+};
+
+const homePage = createNestedElement(page);
+
+export { homePage }
